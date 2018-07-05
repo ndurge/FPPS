c0ac8a40b730911c18f1fd80c502cfc48ed11f82
ndurge <ninadurge@gmail.com>
2018-07-05 10:49:29 +0200

    ja

---
+++ New Unity Project/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    private AudioSource Beau_geluid;
+
+    public AudioClip[] soundClips;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Beau_geluid = (AudioSource)GetComponent (typeof(AudioSource));
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        AudioClip randomClip = soundClips[Random.Range(0, soundClips.Length)];
+
+        Beau_geluid.clip = randomClip;
+        Beau_geluid.Play();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
